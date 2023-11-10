842fc6404b4a3fe9e6d699b180a3ade6d55e5d8a
Jiong <jfadonf@gmail.com>
2023-11-10 22:29:19 +0800

    Portal 100%: finished
        1. the level was established
        2. stepping into the end zone will trigger the display of final message
        3. import the materials
        4. apply materials to the objects

---
+++ projects/portal/Assets/endzone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class endzone : MonoBehaviour {
+
+    public static int isReachTheEnd;
+
+    void Start() {
+        isReachTheEnd = 5;
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        isReachTheEnd = 3;
+	}
+
+}
+
+
+
+
---
+++ projects/portal/Assets/win.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class win : MonoBehaviour
+{
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+        text.text = "You You!";
+
+		// start text off as completely transparent black
+		text.color = new Color(0, 0, 0, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+        // if the player collide with the end collider
+		if (endzone.isReachTheEnd == 3) {
+
+			// reveal text 
+			text.color = new Color(1, 1, 1, 1);
+            text.text = "You Won!";
+        }
+	}
+
+}
